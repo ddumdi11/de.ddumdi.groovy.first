8bd11777dfe7fbf6ac5c64f19bdd6611b64be6f8
Thorsten Diederichs <ddumdi11@gmail.com>
2021-07-03 20:52:27 +0200

    Initialisierung

---
+++ de.ddumdi.groovy.first/README.md
@@ -0,0 +1,4 @@
+Meine Bearbeitung folgender Tutorials:
+
+https://websiteforstudents.com/how-to-install-apache-groovy-on-ubuntu-20-04-18-04/
+https://www.vogella.com/tutorials/Groovy/article.html
---
+++ de.ddumdi.groovy.first/src/de/ddumdi/groovy/first/first/FirstGroovy.groovy
@@ -0,0 +1,9 @@
+package de.ddumdi.groovy.first.first
+
+class FirstGroovy {
+
+	static void main(def args){
+		def mylist= [1, 2, "Diddi", "4", "Hallo Weltchen!"]
+		mylist.each{ println it }
+	}
+}
